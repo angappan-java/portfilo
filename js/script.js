a3b1f4c063b2c0d7ee9b37203c2bdefea32b1280
angappan-java <angappan2399@gmail.com>
2026-05-02 21:38:52 +0530

    script file change

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function sendMessage(event){
       swal("Warning","Enter All Fields!","warning",{buttons:"OK"});
       return;
     }
-    fetch("https://portfolio-backend-production-37fa.up.railway.app/portfolio/contactform",{
+    fetch("http://portfolio-backend-production-37fa.up.railway.app/portfolio/contactform",{
         method:"POST",
         headers:{
             "Content-Type":"application/json"
